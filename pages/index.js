82048df0c7daca7ed384421c1e1f4b955ebac172
Ariyo Halder <ariyohalder@gmail.com>
2022-03-10 12:04:23 +0530

    made some pages

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -13,10 +14,15 @@ export default function Home() {
       </Head>
       <nav className={styles.mainnav}>
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Blog</li>
-            <li>Contact</li>
+            <Link href='/'>
+
+            <a>
+            <li>
+            Home</li></a>
+            </Link>
+            <Link href='/about'><a><li>About</li></a></Link>
+            <Link href='/blog'><a><li>Blog</li></a></Link>
+            <Link href='/contact'><a><li>Contact</li></a></Link>
           </ul>
       </nav>
 
